1537ba8aebd16c3693d521bf9b57f71071f6e66a
Paulo Victor <89660799+paulovictorjg@users.noreply.github.com>
2023-08-24 13:04:14 -0300

    ajuste para regras do jogo bingo conforme luiz

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const letras = ['B', 'I', 'N', 'G', 'O'];
 const colunaNumeros = [[], [], [], [], []];
 const numerosPorColuna = 15;
 const combinacoesBingo = [
-  [0, 1, 2, 3, 4],
   [5, 6, 7, 8, 9],
   [10, 11, 12, 13, 14],
-  [15, 16, 17, 18, 19],
+  [15, 16, 18, 19],
   [20, 21, 22, 23, 24],
-  [0, 5, 10, 15, 20],
-  [1, 6, 11, 16, 21],
-  [2, 7, 12, 17, 22],
-  [3, 8, 13, 18, 23],
-  [4, 9, 14, 19, 24],
-  [0, 4, 20, 24],
+  [25, 26, 27, 28, 29],
+
+  [5, 10, 15, 20, 25],
+  [6, 11, 16, 21, 26],
+  [7, 12, 22, 27],
+  [8, 13, 19, 23, 28],
+  [9, 14, 19, 24, 29],
 ];
 const numerosSorteados = [];
 const numerosClicados = [];
-
 
 for (let i = 0; i < colunaNumeros.length; i++) {
   for (let j = 0; j < numerosPorColuna; j++) {
@@ -62,8 +61,7 @@ function shuffle(array) {
   }
 }
 
-
-// funcao para sortear o numero 
+// funcao para sortear o numero
 
 let indiceAtual = 0;
 
@@ -80,17 +78,15 @@ function sortearNumero() {
   do {
     numeroSorteado = Math.floor(Math.random() * 75) + 1;
   } while (numerosSorteados.includes(numeroSorteado));
+  console.log(numerosSorteados);
 
   numerosSorteados.push(numeroSorteado);
-  console.log(`Sorteando número: ${numeroSorteado}`);
 
   const numerosSorteadosElement = document.querySelector('#n-sorteado');
   numerosSorteadosElement.querySelector('.n-1').textContent = numeroSorteado;
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
-
   const botaoSorteio = document.querySelector('.botao-sorteio');
 
   botaoSorteio.addEventListener('click', () => {
@@ -108,152 +104,54 @@ celulasTabela.forEach((celula) => {
       return;
     }
 
-    if (!numerosClicados.includes(numeroClicado)) {
-      numerosClicados.push(numeroClicado);
-      celula.classList.add('marcados');
+    const celulasTabela = document.querySelectorAll('.numerosCartela');
+    let numerosGanhar = 0;
 
-      // Verificar combinações de bingo aqui
-         for (const combinacao of combinacoesBingo) {
-           const todosNumerosMarcados = combinacao.every((numero) =>
-             numerosClicados.includes(numero)
-           );
-           if (todosNumerosMarcados) {
-             alert('Bingo!');
-             // Continue o jogo ou reinicie como desejar
-             break; // Importante: Para de verificar outras combinações após encontrar um bingo
-           }
-        }
-    }
+    celulasTabela.forEach((celula) => {
+      celula.addEventListener('click', () => {
+        if (!celula.classList.contains('marcados')) {
+          celula.classList.add('marcados');
+          numerosGanhar++;
+          if (Ganhou()) {
+
+            if (numerosGanhar === 5) {
+              if (combinacoesBingo.some((combinacao) => combinacao.includes(numerosGanhar))) {
+              alert('Bingo em uma das combinações!');
+              }
+            } 
+            
+            else if (numerosGanhar === 10) {
+              if (combinacoesBingo.some((combinacao) => combinacao.includes(numerosGanhar))) {
+              alert('Bingo em uma das combinações!');
+              }
+            } 
+            
+            else if (numerosGanhar === 15) {
+              if (combinacoesBingo.some((combinacao) => combinacao.includes(numerosGanhar))) {
+              alert('Bingo em uma das combinações!');
+              }
+            }
+            
+            else if(numerosGanhar === 20) {
+              if (combinacoesBingo.some((combinacao) => combinacao.includes(numerosGanhar))) {
+              alert('Bingo em uma das combinações!');
+              }
+            }
+            
+            else if(numerosGanhar === 24) {
+              alert('B I N G O O O, Você completou toda a Cartela');
+            }
+          };
+        };
+      });
+    });
   });
 });
 
-
-/*const tabela = document.querySelector("#tabelaBingo")
-const bingo = document.querySelector("#nSorteado")
-const combinacoesBingo = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24],
-    [0, 5, 10, 15, 20],
-    [1, 6, 11, 16, 21],
-    [2, 7, 12, 17, 22],
-    [3, 8, 13, 18, 23],
-    [4, 9, 14, 19, 24],
-    [0, 4, 20, 24],
-]
-
-let arrbg = Array.apply(null, { length: 26 }).map(Number.call, Number);
-
-arrbg.shift()
-shuffle(arrbg);
-
-function shuffle(arrbg) {
-    let currentIndex = arrbg.length, randomIndex;
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [arrbg[currentIndex], arrbg[randomIndex]] = [arrbg[randomIndex], arrbg[currentIndex]];
-    }
-    return arrbg;
+    function Ganhou() {
+      return combinacoesBingo.some((combination) => {
+        return combination.every((index) => {
+          return celulasTabela[index].classList.contains('marcados');
+      });
+    });
 }
-let arraytabela = 0;
-for (i = 0; i < 5; i++) {
-    let tr = document.createElement("tr")
-    tabela.appendChild(tr)
-
-    for (j = 0; j < 5; j++) {
-        let td = document.createElement("td")
-        td.id = arrbg[arraytabela].toString()
-        td.style.height = "20%"
-        td.style.width = "20%"
-        td.classList.add("numerosCartela")
-        let div = document.createElement("div")
-        div.classList.add("divCartela")
-        if (i === 2 && j === 2) {
-            div.textContent = "";
-        } else {
-            div.textContent = arrbg[arraytabela].toString();
-        }
-        td.appendChild(div)
-        tr.appendChild(td)
-        arraytabela++;
-    }
-}
-
-function numeroSorteio() {
-    return Math.floor(Math.random() * 50) + 1;
-}
-
-function sortearNumeros() {
-    const numerosSorteados = document.querySelector("#n-sorteado");
-
-    const numero1 = numeroSorteio();
-
-    numerosSorteados.querySelector(".n-1").textContent = numero1;
-
-}
-
-const botaoSorteio = document.querySelector(".botao-sorteio");
-botaoSorteio.addEventListener("click", sortearNumeros);
-
-
-const nBingo = document.querySelectorAll(".numerosCartela");
-let numerosGanhar = 0
-nBingo.forEach(e => {
-    e.addEventListener("click", () => {
-        e.classList.add("marcados");
-
-        if(Ganhou()) {
-            nBingo[numerosGanhar].classList.add("pedras");
-
-            numerosGanhar++;
-            if(numerosGanhar === 1) {
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 2){
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 3){
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 4){
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 5){
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 6){
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 7){
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 8){
-                alert('B I N G O O O');
-            }else if (numerosGanhar === 9){
-                alert('B I N G O O O, Você completou toda a Cartela');
-            }
-        }
-    })
-})
-
-function Ganhou() {
-    const nBingo = document.querySelectorAll(".numerosCartela");
-
-    if (combinacoesBingo.length === 17) {
-        console.log(combinacoesBingo.length === 1)
-
-        alert("Parabens você fechou a cartela, Você venceu o Bingo!");
-    }
-
-    return combinacoesBingo.some(combination => {
-        let verf = 0;
-        combination.forEach(index => {
-            if(nBingo[index].classList.contains("marcados")) verf++;
-        })
-
-        if(verf === 5) {
-            let arrVit = combinacoesBingo.indexOf(combination);
-            combinacoesBingo.splice(arrVit, 1)
-        }
-
-        return combination.every(index => {
-            return nBingo[index].classList.contains("marcados")
-        })
-    })
-}*/
