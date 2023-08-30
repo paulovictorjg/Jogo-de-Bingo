3f2a3297c50218e88ede101104f1f6208fa997a8
Paulo Victor <89660799+paulovictorjg@users.noreply.github.com>
2023-08-30 13:21:31 -0300

    correcao funcao ganhar

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const combinacoesBingo = [
   [15, 16, 18, 19],
   [20, 21, 22, 23, 24],
   [25, 26, 27, 28, 29],
+
   [5, 10, 15, 20, 25],
   [6, 11, 16, 21, 26],
   [7, 12, 22, 27],
   [8, 13, 19, 23, 28],
   [9, 14, 19, 24, 29],
 ];
-
 const numerosSorteados = [];
 const numerosClicados = [];
 
@@ -94,62 +94,49 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+
+//funcao para ganhar
 const celulasTabela = document.querySelectorAll('.numerosCartela');
-
-let numerosGanhar = 0; // Declare fora do loop
-
+let numerosGanhar = 0
 celulasTabela.forEach((celula) => {
   celula.addEventListener('click', () => {
+    const numeroClicado = parseInt(celula.textContent);
+
+    if (!numerosSorteados.includes(numeroClicado)) {
+      alert('O número clicado não foi sorteado.');
+      return;
+    }
+
     if (!celula.classList.contains('marcados')) {
       celula.classList.add('marcados');
-      numerosGanhar++; // Atualize o valor de numerosGanhar
-      console.log(numerosGanhar); // Exiba o valor para fins de depuração
-
+      numerosGanhar++;
+      console.log(numerosGanhar);
       if (Ganhou()) {
         if (numerosGanhar === 5) {
-          if (
-            combinacoesBingo.some((combinacao) =>
-              combinacao.includes(numerosGanhar)
-            )
-          ) {
             alert('Bingo em uma das combinações!');
           }
-        } else if (numerosGanhar === 10) {
-          if (
-            combinacoesBingo.some((combinacao) =>
-              combinacao.includes(numerosGanhar)
-            )
-          ) {
+        else if (numerosGanhar === 10) {
             alert('Bingo em uma das combinações!');
           }
-        } else if (numerosGanhar === 15) {
-          if (
-            combinacoesBingo.some((combinacao) =>
-              combinacao.includes(numerosGanhar)
-            )
-          ) {
+        else if (numerosGanhar === 15) {
             alert('Bingo em uma das combinações!');
           }
-        } else if (numerosGanhar === 20) {
-          if (
-            combinacoesBingo.some((combinacao) =>
-              combinacao.includes(numerosGanhar)
-            )
-          ) {
+        else if (numerosGanhar === 20) {
             alert('Bingo em uma das combinações!');
           }
-        } else if (numerosGanhar === 24) {
+        else if (numerosGanhar === 24) {
           alert('B I N G O O O, Você completou toda a Cartela');
         }
-      }
     }
-  });
+  };
 });
 
-function Ganhou() {
-  return combinacoesBingo.some((combination) => {
-    return combination.every((index) => {
-      return celulasTabela[index].classList.contains('marcados');
+
+    function Ganhou() {
+      return combinacoesBingo.some((combination) => {
+        return combination.every((index) => {
+          return celulasTabela[index].classList.contains('marcados');
+      });
     });
-  });
-}
+  }
+})
